c2c04e221b3d6f47c07f40a026a601192c152f49
Kaiwen Xu <kevin@kevxu.net>
2013-03-17 21:14:54 -0400

    Adding project template.

---
+++ src/com/bingo/eatime/EaTimeServlet.java
@@ -0,0 +1,13 @@
+package com.bingo.eatime;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class EaTimeServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
