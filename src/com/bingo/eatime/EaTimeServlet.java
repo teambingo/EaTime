c6ef8c9992a436ae17110d22758a71e2dad31a03
Kaiwen Xu <kevin@kevxu.net>
2013-03-17 21:23:12 -0400

    Some initial code formatting.

--- src/com/bingo/eatime/EaTimeServlet.java
+++ src/com/bingo/eatime/EaTimeServlet.java
@@ -5,9 +5,11 @@ import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class EaTimeServlet extends HttpServlet {
+
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, world");
 	}
+
 }
