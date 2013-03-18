758b2e1d37ad8933ee6041d19c0df4b9cecf15a1
Kaiwen Xu <kevin@kevxu.net>
2013-03-18 10:45:27 -0400

    Building RestaurantStoreView. Adding favicon to static files.

---
+++ src/com/bingo/eatime/core/RestaurantStoreView.java
@@ -0,0 +1,15 @@
+package com.bingo.eatime.core;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+
+public class RestaurantStoreView {
+
+	private DatastoreService mDatastoreService;
+	
+	public RestaurantStoreView() {
+		this.mDatastoreService = DatastoreServiceFactory.getDatastoreService();
+		
+	}
+
+}
