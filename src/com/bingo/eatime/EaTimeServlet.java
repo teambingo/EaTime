0d7e369262aeff0e96f524ad9388809eedd73cd2
Kaiwen Xu <kevin@kevxu.net>
2013-03-17 21:46:34 -0400

    Minor formatting change and generate serial ID for EaTimeServlet.

--- src/com/bingo/eatime/EaTimeServlet.java
+++ src/com/bingo/eatime/EaTimeServlet.java
@@ -1,10 +1,14 @@
 package com.bingo.eatime;
 
 import java.io.IOException;
-import javax.servlet.http.*;
 
-@SuppressWarnings("serial")
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public class EaTimeServlet extends HttpServlet {
+
+	private static final long serialVersionUID = -7796866550957261709L;
 
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
