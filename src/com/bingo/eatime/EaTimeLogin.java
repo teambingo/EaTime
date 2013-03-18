c49fda40e5e48e5e7491efa665a3f5db214a4b2a
Kaiwen Xu <kevin@kevxu.net>
2013-03-18 10:16:06 -0400

    Reformatting and import statement optimize. Uncapitalize url.

--- src/com/bingo/eatime/EaTimeLogin.java
+++ src/com/bingo/eatime/EaTimeLogin.java
@@ -2,23 +2,23 @@ package com.bingo.eatime;
 
 import java.io.IOException;
 
-import javax.servlet.*;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.appengine.labs.repackaged.org.json.*;
-
+import com.google.appengine.labs.repackaged.org.json.JSONArray;
 
 public class EaTimeLogin extends HttpServlet {
+
+	private static final long serialVersionUID = 4588543340482590495L;
 
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) {
 		resp.setContentType("application/json");
 		System.out.println("Success");
 		String user = req.getParameter("user");
 		String password = req.getParameter("pwd");
-		JSONArray result=new JSONArray();
-		if(user.equals("ryan") && password.equals("crd")){
+		JSONArray result = new JSONArray();
+		if (user.equals("ryan") && password.equals("crd")) {
 			try {
 				result.put("1");
 				resp.getWriter().print(result);
@@ -26,7 +26,7 @@ public class EaTimeLogin extends HttpServlet {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}else{
+		} else {
 			try {
 				result.put("0");
 				resp.getWriter().print(result);
--- src/com/bingo/eatime/EaTimeServlet.java
+++ src/com/bingo/eatime/EaTimeServlet.java
@@ -2,7 +2,7 @@ package com.bingo.eatime;
 
 import java.io.IOException;
 
-import javax.servlet.*;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
