f6dc5de562df49447d3bc6e4aa21364d43dfb425
Kaiwen Xu <kevin@kevxu.net>
2013-03-23 17:38:52 -0400

    Building Person and add md5 to Utilities.

--- src/com/bingo/eatime/core/Person.java
+++ src/com/bingo/eatime/core/Person.java
@@ -1,5 +1,15 @@
 package com.bingo.eatime.core;
 
+import com.google.appengine.api.datastore.Email;
+import com.google.appengine.api.datastore.Key;
+
 public class Person {
+
+	public static final String KIND_PERSON = "person";
+
+	private Key key;
+	private String firstName;
+	private String lastName;
+	private Email email;
 
 }
--- src/com/bingo/eatime/core/Utilities.java
+++ src/com/bingo/eatime/core/Utilities.java
@@ -1,5 +1,8 @@
 package com.bingo.eatime.core;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Utilities {
 
@@ -7,6 +10,31 @@ public class Utilities {
 		String keyName = name.trim().replace(' ', '-').toLowerCase();
 
 		return keyName;
+	}
+
+	public static String hex(byte[] array) {
+		StringBuffer sb = new StringBuffer();
+
+		for (int i = 0; i < array.length; ++i) {
+			sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(
+					1, 3));
+		}
+
+		return sb.toString();
+	}
+
+	public static String md5Hex(String message) {
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+
+			return hex(md.digest(message.getBytes("CP1252")));
+		} catch (NoSuchAlgorithmException e) {
+
+		} catch (UnsupportedEncodingException e) {
+
+		}
+
+		return null;
 	}
 
 }
