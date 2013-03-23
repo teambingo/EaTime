281b3b2fd41e9460dd356859915c6cb1e2ac977d
Kaiwen Xu <kevin@kevxu.net>
2013-03-22 21:44:07 -0400

    Building Restaurant.

--- src/com/bingo/eatime/core/Restaurant.java
+++ src/com/bingo/eatime/core/Restaurant.java
@@ -1,6 +1,7 @@
 package com.bingo.eatime.core;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Restaurant {
@@ -9,5 +10,71 @@ public class Restaurant {
 	private List<Category> categories;
 	private String location;
 	private File image;
+
+	public String getName() {
+		return name;
+	}
+
+	public Restaurant setName(String name) {
+		this.name = name;
+		
+		return this;
+	}
+
+	public List<Category> getCategories() {
+		return categories;
+	}
+
+	public Restaurant setCategories(List<Category> categories) {
+		this.categories = categories;
+		
+		return this;
+	}
+
+	public Restaurant addCategory(Category category) {
+		if (this.categories == null) {
+			this.categories = new ArrayList<Category>();
+		}
+		
+		return this;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public Restaurant setLocation(String location) {
+		this.location = location;
+		
+		return this;
+	}
+
+	public File getImage() {
+		return image;
+	}
+
+	/**
+	 * Set image of this restaurant.
+	 * 
+	 * @param image
+	 *            File object representing the image file in the file system.
+	 */
+	public Restaurant setImage(File image) {
+		this.image = image;
+		
+		return this;
+	}
+
+	/**
+	 * Set image of this restaurant.
+	 * 
+	 * @param image
+	 *            String of the image file path in the file system.
+	 */
+	public Restaurant setImage(String image) {
+		this.image = new File(image);
+		
+		return this;
+	}
 
 }
