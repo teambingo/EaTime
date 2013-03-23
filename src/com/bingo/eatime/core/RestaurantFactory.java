d5532e9d8ab74d91b0ecdd7fa42d1a6b307a8a80
Kaiwen Xu <kevin@kevxu.net>
2013-03-22 21:49:43 -0400

    Building RestaurantFactory.

--- src/com/bingo/eatime/core/RestaurantFactory.java
+++ src/com/bingo/eatime/core/RestaurantFactory.java
@@ -1,7 +1,42 @@
 package com.bingo.eatime.core;
 
+import java.io.File;
+import java.util.List;
+
 public class RestaurantFactory {
-	
-	
+
+	public static Restaurant createRestaurant(String name,
+			List<Category> categories, String location, File image) {
+		Restaurant restaurant = new Restaurant().setName(name)
+				.setCategories(categories).setLocation(location)
+				.setImage(image);
+
+		return restaurant;
+	}
+
+	public static Restaurant createRestaurant(String name,
+			List<Category> categories, String location, String image) {
+		Restaurant restaurant = new Restaurant().setName(name)
+				.setCategories(categories).setLocation(location)
+				.setImage(image);
+
+		return restaurant;
+	}
+
+	public static Restaurant createRestaurant(String name, Category category,
+			String location, File image) {
+		Restaurant restaurant = new Restaurant().setName(name)
+				.addCategory(category).setLocation(location).setImage(image);
+
+		return restaurant;
+	}
+
+	public static Restaurant createRestaurant(String name, Category category,
+			String location, String image) {
+		Restaurant restaurant = new Restaurant().setName(name)
+				.addCategory(category).setLocation(location).setImage(image);
+
+		return restaurant;
+	}
 
 }
