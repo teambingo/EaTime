c00dde94384b52bf1fbc100ef90c54c2fac332a1
Kaiwen Xu <kevin@kevxu.net>
2013-03-23 18:55:06 -0400

    Building Person.

--- src/com/bingo/eatime/core/Person.java
+++ src/com/bingo/eatime/core/Person.java
@@ -1,7 +1,12 @@
 package com.bingo.eatime.core;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.bingo.eatime.util.Gravatar;
 import com.google.appengine.api.datastore.Email;
 import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.Link;
 
 public class Person {
 
@@ -20,32 +25,66 @@ public class Person {
 		return key;
 	}
 
-	private void setKey(Key key) {
+	private Person setKey(Key key) {
 		this.key = key;
+
+		return this;
 	}
 
 	public String getFirstName() {
 		return firstName;
 	}
 
-	private void setFirstName(String firstName) {
+	private Person setFirstName(String firstName) {
 		this.firstName = firstName;
+
+		return this;
 	}
 
 	public String getLastName() {
 		return lastName;
 	}
 
-	private void setLastName(String lastName) {
+	private Person setLastName(String lastName) {
 		this.lastName = lastName;
+
+		return this;
 	}
 
 	public Email getEmail() {
 		return email;
 	}
 
-	private void setEmail(Email email) {
+	private Person setEmail(Email email) {
 		this.email = email;
+
+		return this;
+	}
+
+	public String getFullName() {
+		return firstName + " " + lastName;
+	}
+
+	public String getGravatarUrlString() {
+		return Gravatar.getGravatarUrlString(email.getEmail(), true, 200);
+	}
+
+	public URL getGravatarUrl() {
+		try {
+			URL url = new URL(getGravatarUrlString());
+
+			return url;
+		} catch (MalformedURLException e) {
+
+		}
+
+		return null;
+	}
+
+	public Link getGravatarUrlLink() {
+		Link link = new Link(getGravatarUrlString());
+
+		return link;
 	}
 
 }
