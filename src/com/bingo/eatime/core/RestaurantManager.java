0454e2f508953ce70b39e95522d12c03682033ff
Kaiwen Xu <kevin@kevxu.net>
2013-03-22 21:27:52 -0400

    Several class rename.

--- src/com/bingo/eatime/core/RestaurantManager.java
+++ src/com/bingo/eatime/core/RestaurantManager.java
@@ -4,19 +4,14 @@ import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 
-public class RestaurantStore {
+public class RestaurantManager {
 
 	public static final String KIND_RESTAURANT = "restaurant";
 
 	private DatastoreService mDatastoreService;
 
-	public RestaurantStore() {
+	public RestaurantManager() {
 		mDatastoreService = DatastoreServiceFactory.getDatastoreService();
-
-	}
-
-	public void add() {
-		
 
 	}
 
