7a14fc62c419ceb9ed533302248d2d1126019e9e
Kaiwen Xu <kevin@kevxu.net>
2013-03-23 21:44:03 -0400

    Built addPerson.

--- src/com/bingo/eatime/core/PersonManager.java
+++ src/com/bingo/eatime/core/PersonManager.java
@@ -2,12 +2,38 @@ package com.bingo.eatime.core;
 
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.Transaction;
 
 public class PersonManager {
-	
+
 	public static Key addPerson(Person person) {
-		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		DatastoreService datastore = DatastoreServiceFactory
+				.getDatastoreService();
+
+		Transaction txn = datastore.beginTransaction();
+		Key personKey;
+		try {
+			Entity personEntity = new Entity(Person.KIND_PERSON);
+			personEntity.setProperty(Person.PROPERTY_FIRSTNAME,
+					person.getFirstName());
+			personEntity.setProperty(Person.PROPERTY_LASTNAME,
+					person.getLastName());
+			personEntity.setProperty(Person.PROPERTY_EMAIL, person.getEmail());
+
+			personKey = datastore.put(personEntity);
+
+			txn.commit();
+		} finally {
+			if (txn.isActive()) {
+				txn.rollback();
+
+				return null;
+			}
+		}
+
+		return personKey;
 	}
 
 }
