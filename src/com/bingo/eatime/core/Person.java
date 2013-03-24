ea4b3e373d56c6d0dc846f751eff544d22c91367
Kaiwen Xu <kevin@kevxu.net>
2013-03-23 20:19:04 -0400

    Building Person.

--- src/com/bingo/eatime/core/Person.java
+++ src/com/bingo/eatime/core/Person.java
@@ -7,12 +7,17 @@ import org.apache.commons.lang3.text.WordUtils;
 
 import com.bingo.eatime.util.Gravatar;
 import com.google.appengine.api.datastore.Email;
+import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.Link;
 
 public class Person {
 
 	public static final String KIND_PERSON = "person";
+
+	public static final String PROPERTY_FIRSTNAME = "firstname";
+	public static final String PROPERTY_LASTNAME = "lastname";
+	public static final String PROPERTY_EMAIL = "email";
 
 	private Key key;
 	private String firstName;
@@ -87,6 +92,34 @@ public class Person {
 		Link link = new Link(getGravatarUrlString());
 
 		return link;
+	}
+
+	public static Person createPerson(String firstName, String lastName,
+			String email) {
+		return createPerson(firstName, lastName, new Email(email));
+	}
+
+	public static Person createPerson(String firstName, String lastName,
+			Email email) {
+		Person person = new Person();
+		person.setFirstName(firstName).setLastName(lastName).setEmail(email);
+
+		return person;
+	}
+
+	public static Person createPerson(Entity entity) {
+		if (entity.getKind().equals(KIND_PERSON)) {
+			Person person = new Person();
+			person.setKey(entity.getKey());
+			person.setFirstName((String) entity.getProperty(PROPERTY_FIRSTNAME));
+			person.setLastName((String) entity.getProperty(PROPERTY_LASTNAME));
+			person.setEmail((Email) entity.getProperty(PROPERTY_EMAIL));
+
+			return person;
+		} else {
+			throw new EntityKindNotMatchException(
+					"Entity Kind must be KIND_PERSON.");
+		}
 	}
 
 }
