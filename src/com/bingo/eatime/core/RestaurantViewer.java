b30954bc830e354b8d3594700cfcaaaf8d0c9a15
Kaiwen Xu <kevin@kevxu.net>
2013-03-24 03:08:40 -0400

    Testing database.

--- src/com/bingo/eatime/core/RestaurantViewer.java
+++ src/com/bingo/eatime/core/RestaurantViewer.java
@@ -1,15 +1,6 @@
 package com.bingo.eatime.core;
 
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
 
 public class RestaurantViewer {
-
-	private DatastoreService mDatastoreService;
-
-	public RestaurantViewer() {
-		mDatastoreService = DatastoreServiceFactory.getDatastoreService();
-
-	}
 
 }
--- src/com/bingo/eatime/test/EaTimeDatabaseTestServlet.java
+++ src/com/bingo/eatime/test/EaTimeDatabaseTestServlet.java
@@ -1,8 +1,18 @@
 package com.bingo.eatime.test;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.bingo.eatime.core.Category;
+import com.bingo.eatime.core.CategoryManager;
+import com.bingo.eatime.core.Restaurant;
+import com.bingo.eatime.core.RestaurantManager;
+import com.google.appengine.api.datastore.PhoneNumber;
+import com.google.appengine.api.datastore.PostalAddress;
 
 public class EaTimeDatabaseTestServlet extends HttpServlet {
 
@@ -10,7 +20,32 @@ public class EaTimeDatabaseTestServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
-		
+		try {
+			resp.setContentType("text/html");
+			PrintWriter writer = resp.getWriter();
+			writer.println("<html><head></head><body><p>Testing Database...</p></body></html>");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		testDb();
+	}
+
+	private void testDb() {
+		boolean result;
+
+		Category categoryChinese = Category.createCategory("Chinese");
+		result = CategoryManager.addCategory(categoryChinese);
+
+		System.out.println("Add category " + result);
+
+		Restaurant restaurantHappyChina = Restaurant.createRestaurant(
+				"Happy China", categoryChinese, new PostalAddress(
+						"219 E State Street, West Lafayette, IN 47906"),
+				new PhoneNumber("765-743-1666"));
+		result = RestaurantManager.addRestaurant(restaurantHappyChina);
+
+		System.out.println("Add restaurant " + result);
 	}
 
 }
