a713272a060cfe6d97df1f2cf4b44967f1380228
Kaiwen Xu <kevin@kevxu.net>
2013-03-23 21:19:56 -0400

    Add NullKeyException.

---
+++ src/com/bingo/eatime/core/NullKeyException.java
@@ -0,0 +1,23 @@
+package com.bingo.eatime.core;
+
+public class NullKeyException extends RuntimeException {
+
+	private static final long serialVersionUID = 7616839790577627916L;
+
+	public NullKeyException() {
+
+	}
+
+	public NullKeyException(String message) {
+		super(message);
+	}
+
+	public NullKeyException(Throwable cause) {
+		super(cause);
+	}
+
+	public NullKeyException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
