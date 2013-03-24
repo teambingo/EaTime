1c1b61ba27ea68d38949fc3dfa7c4cfaa781189f
Kaiwen Xu <kevin@kevxu.net>
2013-03-23 21:58:22 -0400

    Building addInvite(s).

--- src/com/bingo/eatime/core/EventManager.java
+++ src/com/bingo/eatime/core/EventManager.java
@@ -1,5 +1,7 @@
 package com.bingo.eatime.core;
 
+import java.util.HashSet;
+
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
@@ -51,6 +53,41 @@ public class EventManager {
 		}
 
 		return eventKey;
+	}
+
+	public static boolean addInvite(Person person, Key eventKey) {
+		HashSet<Person> people = new HashSet<Person>();
+		people.add(person);
+		return addInvites(people, eventKey);
+	}
+
+	public static boolean addInvites(Iterable<Person> people, Key eventKey) {
+		DatastoreService datastore = DatastoreServiceFactory
+				.getDatastoreService();
+
+		Transaction txn = datastore.beginTransaction();
+		try {
+			for (Person person : people) {
+				Key personKey = person.getKey();
+				if (personKey == null) {
+					txn.rollback();
+					throw new NullKeyException("Person Key is null.");
+				}
+
+				Entity personKeyEntity = createPersonKeyEntity(personKey,
+						eventKey);
+				datastore.put(personKeyEntity);
+			}
+			txn.commit();
+		} finally {
+			if (txn.isActive()) {
+				txn.rollback();
+
+				return false;
+			}
+		}
+
+		return true;
 	}
 
 	protected static Entity createPersonKeyEntity(Key personKey, Key eventKey) {
