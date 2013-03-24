16ea2e5f6d0f0b07206beb4b6d2abe5bfa670c7e
Kaiwen Xu <kevin@kevxu.net>
2013-03-24 02:30:27 -0400

    Add EaTimeDatabaseTestServlet.

---
+++ src/com/bingo/eatime/test/EaTimeDatabaseTestServlet.java
@@ -0,0 +1,16 @@
+package com.bingo.eatime.test;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class EaTimeDatabaseTestServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 2639400166713434665L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+
+	}
+
+}
