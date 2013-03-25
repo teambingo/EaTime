219b637f1dddb624e20bb8cf56382c088bf7245b
Rendong Chen <chenrd769@gmail.com>
2013-03-25 19:46:55 -0400

    update css and js and img

--- war/js/main.js
+++ war/js/main.js
@@ -1,5 +1,10 @@
 var isModalOn=false;
 $(function(){
+	$('.timepick').timepicker({
+   		altField: '.timepick',
+   		defaultTime: '9:20'
+	});
+	
 	$('#myTab a').click(function (e) {
   		e.preventDefault();
   		$(this).tab('show');
