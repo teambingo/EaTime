57bae5d817d9d6f9a64601a93e8ec9935adf0957
Kaiwen Xu <kevin@kevxu.net>
2013-03-28 16:04:28 -0400

    Add commons-codec to library.

--- src/com/bingo/eatime/core/UserAccount.java
+++ src/com/bingo/eatime/core/UserAccount.java
@@ -1,5 +1,11 @@
 package com.bingo.eatime.core;
 
+import com.google.appengine.api.datastore.Key;
+
 public class UserAccount extends Person {
+	
+	public static final String KIND_USERACCOUNT = "account";
+	
+	private Key key;
 
 }
