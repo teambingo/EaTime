bda590a21bfc5192bc6c8adb8b1f2e998b5ab6c3
Kaiwen Xu <kevin@kevxu.net>
2013-03-28 16:21:47 -0400

    Add RestaurantAdminServlet.

---
+++ src/com/bingo/eatime/admin/RestaurantAdminServlet.java
@@ -0,0 +1,15 @@
+package com.bingo.eatime.admin;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RestaurantAdminServlet extends HttpServlet {
+
+	private static final long serialVersionUID = -7332305846068735050L;
+
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) {
+		
+	}
+
+}
