9a3044dc5cc026320d0088c50935569f0f5cf520
Kaiwen Xu <kevin@kevxu.net>
2013-03-30 18:17:16 -0400

    Minor change.

---
+++ src/com/bingo/eatime/admin/EventAdminServlet.java
@@ -0,0 +1,15 @@
+package com.bingo.eatime.admin;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class EventAdminServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 8208066016833205326L;
+
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) {
+		
+	}
+
+}
