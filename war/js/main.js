0b2fbef1cadce22b4112f340200e34671f47ab49
Kaiwen Xu <kevin@kevxu.net>
2013-03-31 02:19:23 -0400

    Improved comma splitting.

--- war/js/main.js
+++ war/js/main.js
@@ -2,13 +2,13 @@ var isModalOn = false;
 
 $(function() {
 	$(".error").css("visibility","hidden");
+
 	$('.timepick').timepicker({
 		altField: '.timepick',
 		defaultTime: '12:00'
 	});
 
 	$('#myTab a').click(function (e) {
-		
 		e.preventDefault();
 		$(this).tab('show');
 	});
@@ -35,33 +35,39 @@ $(function() {
 	});
 
 	$("#create").click(function(){
-		var name=$("#event-name").val();
-		var time=$(".timepick").timepicker('getTime');
-		var hourandmin=time.split(":");
-		var d=new Date();
+		var name = $("#event-name").val();
+
+		var time = $(".timepick").timepicker('getTime');
+		var hourandmin = time.split(":");
+		var d = new Date();
 		d.setHours(hourandmin[0]);
 		d.setMinutes(hourandmin[1]);
 		d.setSeconds(0);
 		d.setMilliseconds(0);
-		var date=d.getTime();
-		var invites=$("#event-invite").val().split(',');
+		var date = d.getTime();
+
+		var invites = $("#event-invite").val().split(/[\s,]+/);
 		for (var i = 0;  i < invites.length; i++) {
-			alert(invites[i]);
+			console.log(invites[i]);
 		}
-		if(name==''){
+
+		if (name === '') {
 			$("#event-name").css("border","1px solid #C83135");
 			$(".error").css("visibility","visible");
-		}else{
-			var url="event?";
-			url+="action=create";
-			url+="&name="+name;
-			url+="&restaurant="+$(this).data("restaurant");
-			url+="&date="+date;
-			if($("#event-invite").val()!=''){
-				for (var i = 0;  i < invites.length; i++) {
-					url+="&invite="+invites[i];
+		} else {
+			var url = "event?";
+			url += "action=add";
+			url += "&name=" + name;
+			url += "&restaurant=" + $(this).data("restaurant");
+			url += "&date=" + date;
+			if ($("#event-invite").val() !== '') {
+				for (i = 0;  i < invites.length; i++) {
+					url += "&invite=" + invites[i];
 				}
 			}
+
+
+
 			window.location.href = "event";
 		}
 	});
