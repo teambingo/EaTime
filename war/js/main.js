639f0df29305beffc27c925b71f5241214dcc220
Kaiwen Xu <kevin@kevxu.net>
2013-04-01 13:44:12 -0400

    Minor TODO list update to main.js.

--- war/js/main.js
+++ war/js/main.js
@@ -1,10 +1,11 @@
 var isModalOn = false;
 
 
-//TODO CHANGE img
-function createEvent(restaurant,hour,min){	
+// TODO CHANGE img
+// TODO display user's full name instead of username.
+function createEvent(restaurant,hour,min){
 	var br='<hr>';
-	if($("*[value="+restaurant+"]").next().children('.event').length==0){
+	if($("*[value="+restaurant+"]").next().children('.event').length === 0){
 		br='';
 	}
 	var newEvent=$(br+'<div class="row-fluid event" id=1><div class="span2 headDiv"><img src="http://www.gravatar.com/avatar/7c46aa86b25a0d1e343affd790e10700.jpg?s=100>" class="img-circle head"></div><div class="span3 orgDiv"><div class="label label-info">Organizer</div><div class="display">'+username+'</div></div><div class="span3 timeDiv"><div class="label label-info">Time</div><br><div class="hourNum">'+hour+'</div>:<div class="minNum">'+min+'</div></div><div class="span2 countDiv"><div class="label label-info">Attendants</div><div class="display">0</div></div><div class="span2 joinDiv"><button type="submit" class="btn btn-info join">Join!</button></div>');
