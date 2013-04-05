72e4c56b4ac4ddd31d1c3307ba14a1a508bb31aa
Kaiwen Xu <kevin@kevxu.net>
2013-04-05 00:53:28 -0400

    Building EaTimeDatabaseDemoServlet.

--- src/com/bingo/eatime/test/EaTimeDatabaseDemoServlet.java
+++ src/com/bingo/eatime/test/EaTimeDatabaseDemoServlet.java
@@ -1,11 +1,20 @@
 package com.bingo.eatime.test;
 
+import java.util.Calendar;
+import java.util.HashSet;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.bingo.eatime.core.Category;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Transaction;
 
 public class EaTimeDatabaseDemoServlet extends HttpServlet {
 
@@ -17,13 +26,32 @@ public class EaTimeDatabaseDemoServlet extends HttpServlet {
 	}
 	
 	private synchronized void buildDemo() {
-		cleanDatabase();
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		Transaction txn = datastore.beginTransaction();
+
+		cleanDatabase(txn);
+		addCategories(txn);
+		
+		txn.commit();
 	}
 	
-	private synchronized void cleanDatabase() {
+	private synchronized void cleanDatabase(Transaction txn) {
 		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 		
+		HashSet<Key> allKeys = new HashSet<Key>();
 		
+		Query q = new Query();
+		PreparedQuery pq = datastore.prepare(txn, q);
+		
+		for (Entity entity : pq.asIterable()) {
+			allKeys.add(entity.getKey());
+		}
+		
+		datastore.delete(txn, allKeys);
+	}
+	
+	private synchronized void addCategories(Transaction txn) {
+		Category american = Category.createCategory("American");
 	}
 
 }
