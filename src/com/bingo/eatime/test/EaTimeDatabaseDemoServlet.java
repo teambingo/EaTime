38e8b4ba9883386a70c9c1b9b02cc66324dbb270
Kaiwen Xu <kevin@kevxu.net>
2013-04-05 00:30:16 -0400

    Building EaTimeDatabaseDemoServlet.

--- src/com/bingo/eatime/test/EaTimeDatabaseDemoServlet.java
+++ src/com/bingo/eatime/test/EaTimeDatabaseDemoServlet.java
@@ -4,13 +4,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+
 public class EaTimeDatabaseDemoServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+		buildDemo();
+	}
 	
+	private synchronized void buildDemo() {
+		cleanDatabase();
+	}
+	
+	private synchronized void cleanDatabase() {
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		
+		
 	}
 
 }
